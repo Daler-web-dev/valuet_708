4423be7ef87fa13a3ffd6f9a0d69c09309d026c9
Amin <107203714+0AmIn3@users.noreply.github.com>
2022-11-10 16:16:50 +0500

    last patch

--- index.js
+++ index.js
@@ -45,20 +45,18 @@ data.innerHTML = date.getDate() + ' ' + str[date.getMonth()] + ' , ' + arr[date.
 
 
 
-const ctx = document.getElementById('doughnut').getContext('2d');
-const myChart = new Chart(ctx, {
+const ctx_1 = document.getElementById('doughnut').getContext('2d');
+const myChart = new Chart(ctx_1, {
     type: 'doughnut',
     data: {
         // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
+            label: ' ',
             data: [18, 64, 18],
             backgroundColor: [
                 '#F5FBFE',
                 '#018FFF',
                 '#FAD679',
-
-
             ],
             borderColor: [
 
@@ -68,9 +66,70 @@ const myChart = new Chart(ctx, {
     },
 
 });
-myChart.config._config.data.datasets[0].backgroundColor
-console.log();
 
+const ctx = document.getElementById('line').getContext('2d');
+const myChart_1 = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [
+         
+            date.getDate() -7 + ' ' + str[date.getMonth()],
+            date.getDate() -6 + ' ' + str[date.getMonth()],
+            date.getDate() -5 + ' ' + str[date.getMonth()],
+            date.getDate() -4 + ' ' + str[date.getMonth()],
+            date.getDate() -3 + ' ' + str[date.getMonth()],
+            date.getDate() -2 + ' ' + str[date.getMonth()],
+            date.getDate() -1 + ' ' + str[date.getMonth()],
+            date.getDate() -0 + ' ' + str[date.getMonth()]],
+        datasets: [{
+            label: ' ',
+            data: [12, 19, 3, 5, 2, 3 , 12 , 15 , 6],
+            backgroundColor: [
+                '#018FFF',
+
+            ],
+            borderColor: [
+                '#0097E8',
+
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
+});
+const ctx_2 = document.getElementById('line_2').getContext('2d');
+const myChart_2 = new Chart(ctx_2, {
+    type: 'line',
+    data: {
+        labels: [ ' ' , ' ' ,' ',' ',' ',' ',' ',' '],
+        datasets: [{
+            label: ' ',
+            data: [12, 19, 3, 5, 2, 3 , 12 , 15 , 6],
+            backgroundColor: [
+                '#018FFF',
+
+            ],
+            borderColor: [
+                '#0097E8',
+
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
+});
 let color = document.querySelectorAll('#create')
 let procent = document.querySelectorAll('.procent')
 let colors = myChart.config._config.data.datasets[0].backgroundColor
@@ -84,9 +143,20 @@ color.forEach(col => {
 })
 
 
+
+
 // let coin = [
 //     {
 //         name: 'BitCoin',
             
 //     }
 // ]
+
+
+
+
+
+
+
+
+
